d3a823db2074c7a23300c2e9b03ff83a2fcf0fea
zeokav <sakettsingh@outlook.com>
2017-04-09 05:28:23 +0530

    Added actions and stuff!

--- Script/script.py
+++ Script/script.py
@@ -6,6 +6,10 @@ import json
 import random
 from subprocess import call
 
+data_plans = [["0.1GB Booster Plans", 11 ], ["1GB Booster Plans", 51], ["2GB Booster Plans", 91]]
+calling_plans = [["199 Super Saver", 199], ["299 Super Saver", 299], ["399 Super Saver", 399]]
+already_in_use_numbers = ["9829055445", "9731794141", "8954012345","9663595184"]
+
 def say(msg):
     call(["espeak", '-ven+f3', msg], shell=True)
 
@@ -34,16 +38,45 @@ def record():
         pass
 
 def send_to_wit(input_str, session):
-    while True:
-        response = requests.post(url='https://api.wit.ai/converse?v=20170408&session_id='+session+'&q='+input_str,headers={ "Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer Y6V5XF5UNI2ETGKO3NUJLWANYKUKJIJ2"})
+    response = requests.post(url='https://api.wit.ai/converse?v=20170408&session_id='+session+'&q='+input_str,headers={ "Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer Y6V5XF5UNI2ETGKO3NUJLWANYKUKJIJ2"})
+    dict_response = json.loads(response.text)
+    print (dict_response)
+    while dict_response['type']!='stop':
+        # if(dict_response['entities']['operation']):
+        # print (dict_response['entities']['operation'])
+        # for data in dict_response['entities']:
+        #     print (data)
+        #     for i in dict_response['entities'][data]:
+        #         print (i['value'])
+
+        if dict_response['type'] == 'action':
+            try:
+                if dict_response['action'] == 'updateNumber':
+                    print ("ysasf")
+                    inp_number = dict_response['entities']['phone_number'][0]['value']
+                    inp_number.replace(" ", "")
+                    if inp_number not in already_in_use_numbers:
+                        print ("yes")
+                        response = requests.post(url='https://api.wit.ai/converse?v=20170408&session_id='+session,headers={ "Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer Y6V5XF5UNI2ETGKO3NUJLWANYKUKJIJ2"}, json={"successful":"True"})
+                        dict_response = json.loads(response.text)
+                        print (dict_response)
+                    else:
+                        print ("no")
+                        response = requests.post(url='https://api.wit.ai/converse?v=20170408&session_id='+session,headers={ "Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer Y6V5XF5UNI2ETGKO3NUJLWANYKUKJIJ2"}, json={"notSuccessful":"True"})
+                        dict_response = json.loads(response.text)
+                        print (dict_response)
+            except Exception as e:
+                print ("Exception in action")
+
+        if dict_response['type'] == 'msg': 
+            try:
+                say(dict_response['msg'])
+            except Exception as e:
+                print ("Exception in message")
+        
+        response = requests.post(url='https://api.wit.ai/converse?v=20170408&session_id='+session,headers={ "Content-Type": "application/json","Accept": "application/json","Authorization": "Bearer Y6V5XF5UNI2ETGKO3NUJLWANYKUKJIJ2"})
         dict_response = json.loads(response.text)
         print (dict_response)
-        if(dict_response['type'] == 'stop'):
-            return
-        try:
-            say(dict_response['msg'])
-        except Exception as e:
-            print (e)
 
 if __name__ == "__main__":
     record()
